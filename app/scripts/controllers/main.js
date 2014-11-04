a9a533013ceb7f4d3dafcb869ae23aabb3575a9e
zhang-yuan <yzgg1020@gmail.com>
2014-11-04 21:34:58 +0800

    update unix timestamp to date

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,7 @@
 angular.module('angularWeatherApp')
   .controller('MainCtrl', function($scope, $http) {
     $scope.title = "Weather";
+    $scope.unitFlag = "C"
 
     $scope.searchWeather = function() {
       var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?cnt=7&q=" + $scope.city + "&callback=JSON_CALLBACK&lang=" + "zh_cn" + "&units=" + "metric" + "&APPID=1bb8aaab54671b48242f9a892b6c0374";
